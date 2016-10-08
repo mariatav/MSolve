a79cc8f64c025f4b2e4d8f1f7ed43a4adc1cef56
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 01:44:11 +0300

    Create classes ElasticityTensorContinuum3D, StressStrainVectorContinuum3D
    Class ElasticityTensorContinuum3D inherits from Matrix2D and applies the constraint that the matrix dimensions are 6 by 6
    Class StressStrainVectorContinuum3D inherits from Vector and applies the constraint that the vector dimensions is 6

---
+++ ISAAR.MSolve.Matrices/ElasticityTensorContinuum3D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Matrices
+{
+    public class ElasticityTensorContinuum3D : Matrix2D<Double>
+    {
+        public ElasticityTensorContinuum3D() : base(6, 6) { }
+
+        public ElasticityTensorContinuum3D(double[,] data) : base(data)
+        {
+            if (data.GetLength(0) != 6 || data.GetLength(1) != 6) throw new ArgumentException(String.Format("input array (dimensions: {0} by {1}) is not 6 by 6", data.GetLength(0), data.GetLength(1)));
+        }
+    }
+}
---
+++ ISAAR.MSolve.Matrices/StressStrainVectorContinuum3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.Matrices
+{
+    public class StressStrainVectorContinuum3D : Vector<Double>
+    {
+        public StressStrainVectorContinuum3D() : base(6) { }
+        public StressStrainVectorContinuum3D(double[] data) : base(data)
+        {
+            if (data.Length != 6) throw new ArgumentException(String.Format("input array dimension ({0}) is not 6", data.Length));
+        }
+    }
+}

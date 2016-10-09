ea894e8b42ccb1b7e808eb1b56e6478e15fe5c3a
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-09 17:31:57 +0300

    Create interface IContinuumMaterial2D
    This interface is meant to be used by 2d plane stress or plane strain elements. Its members are similar to the IContinuumMaterial3D interface.

---
+++ ISAAR.MSolve.PreProcessor/Interfaces/IContinuumMaterial2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ISAAR.MSolve.Matrices;
+
+namespace ISAAR.MSolve.PreProcessor.Interfaces
+{
+    public interface IContinuumMaterial2D : IFiniteElementMaterial
+    {
+        StressStrainVectorContinuum2D Stresses { get; }
+        ElasticityTensorContinuum2D ConstitutiveMatrix { get; }
+        void UpdateMaterial(StressStrainVectorContinuum2D strains);
+        void ClearState();
+        void SaveState();
+        void ClearStresses();
+    }
+}

9cd557c6bd98f1290cef5c036db6629f36db15fd
MARIA-PC\maria <mariataylaki@gmail.com>
2016-10-24 19:15:14 +0300

    Add BuildingInNoSoilSmall example

---
+++ ISAAR.MSolve.SamplesConsole/Static/Linear/BuildingInNoSoilSmall.cs
@@ -0,0 +1,45 @@
+﻿using ISAAR.MSolve.Analyzers;
+using ISAAR.MSolve.Logging;
+using ISAAR.MSolve.Matrices;
+using ISAAR.MSolve.PreProcessor;
+using ISAAR.MSolve.Problems;
+using ISAAR.MSolve.Solvers.Skyline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.SamplesConsole.Static.Linear
+{
+    class BuildingInNoSoilSmall
+    {
+        public void Check()
+        {
+            VectorExtensions.AssignTotalAffinityCount();
+            Model model = new Model();
+            model.SubdomainsDictionary.Add(1, new Subdomain() { ID = 1 });
+            BeamBuildingBuilder.MakeBeamBuilding(model, 20, 20, 20, 5, 4, model.NodesDictionary.Count + 1,
+                model.ElementsDictionary.Count + 1, 1, 4, false, false);
+            model.Loads.Add(new Load() { Amount = -100, Node = model.Nodes[21], DOF = DOFType.X });
+            model.ConnectDataStructures();
+
+            SolverSkyline solver = new SolverSkyline(model);
+            ProblemStructural provider = new ProblemStructural(model, solver.SubdomainsDictionary);
+            LinearAnalyzer analyzer = new LinearAnalyzer(solver, solver.SubdomainsDictionary);
+            StaticAnalyzer parentAnalyzer = new StaticAnalyzer(provider, analyzer, solver.SubdomainsDictionary);
+
+            analyzer.LogFactories[1] = new LinearAnalyzerLogFactory(new int[] { 420 });
+
+            parentAnalyzer.BuildMatrices();
+            parentAnalyzer.Initialize();
+            parentAnalyzer.Solve();
+            Dictionary<int, double> results = (analyzer.Logs[1][0] as DOFSLog).DOFValues;
+            double expected = -0.189287317648762;
+            if (Math.Abs(expected - results[420]) > 1e-15)
+            {
+                throw new SystemException("Failed BuildingInNoSoilSmall test, expected displacement: " + expected + ", calculated displacement: " + results[0]);
+            }
+            Console.WriteLine("ran BuildingInNoSoilSmall test");
+        }
+    }
+}

6a6f2fe0ae937f2f1c23780b2b89d1c167e8c556
Maria Tavlaki <mariataylaki@gmail.com>
2016-12-20 20:10:00 +0200

    Remove commented out Element code, rename Element.cs file to AbsorptionType

---
+++ ISAAR.MSolve.PreProcessor/AbsorptionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ISAAR.MSolve.PreProcessor.Interfaces;
+using ISAAR.MSolve.Matrices.Interfaces;
+
+namespace ISAAR.MSolve.PreProcessor
+{
+    public enum AbsorptionType
+    {
+        Unknown = 0,
+        Compressional = 1,
+        Shear = 2
+    }
+}
